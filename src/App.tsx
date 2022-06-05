f10408b7cdd30ce2d73be58c1990322320c7eba4
AlexandrMasalitin <masalitindev@gmail.com>
2022-06-05 13:18:05 +0300

    Chess Finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,58 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from 'react';
 import "./App.css";
-import BoardComponent from "./component/BoardComponent";
-import { Board } from "./models/Board";
+import BoardComponent from "../src/component/BoardComponent";
+import {Board} from "./models/Board";
+import {Player} from "./models/Player";
+import {Colors} from "./models/Colors";
+import LostFigures from "../src/component/LostFigures";
+import Timer from "../src/component/Timer";
 
 const App = () => {
-  const [board, setBoard] = useState(new Board ())
+  const [board, setBoard] = useState(new Board())
+  const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE))
+  const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK))
+  const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null);
 
-useEffect( () => {
-  restart()
-},[])
+  useEffect(() => {
+    restart()
+    setCurrentPlayer(whitePlayer);
+  }, [])
 
-  function restart(){
+  function restart() {
     const newBoard = new Board();
-    newBoard.initCell()
+    newBoard.initCells()
+    newBoard.addFigures()
     setBoard(newBoard)
   }
 
+  function swapPlayer() {
+    setCurrentPlayer(currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer)
+  }
 
-  return(
+  return (
     <div className="app">
-     <BoardComponent 
-     board={board}
-     setBoard={setBoard}
-     />
+      <Timer
+        restart={restart}
+        currentPlayer={currentPlayer}
+      />
+      <BoardComponent
+        board={board}
+        setBoard={setBoard}
+        currentPlayer={currentPlayer}
+        swapPlayer={swapPlayer}
+      />
+      <div>
+        <LostFigures
+          title="Черные фигуры"
+          figures={board.lostBlackFigures}
+        />
+        <LostFigures
+          title="Белые фигуры"
+          figures={board.lostWhiteFigures}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
+
 export default App
